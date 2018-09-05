67f5f4bad8d65dea9d0a1c8b815b616873e3a093
git <git@1885474.no-reply.drupal.org>
2018-09-05 10:22:06 -0400

    Issue #2969120 by xserjiox, welly, irenebreiner, Sissonen: Add Geofield support for Feeds D8 - amended to geofield-add-feeds-support-269120-17.patch, to comply to Drupal standards

--- src/Feeds/Target/Geofield.php
+++ src/Feeds/Target/Geofield.php
@@ -24,65 +24,67 @@ class Geofield extends FieldTargetBase {
     $definition = FieldTargetDefinition::createFromFieldDefinition($field_definition)
       ->addProperty('lat')
       ->addProperty('lon');
-      return $definition;
+    return $definition;
   }
 
   /**
    * {@inheritdoc}
    */
   protected function prepareValues(array $values) {
-      $return = array();
-      $coordinates = array();
-      $coordinates_counter = 0;
+    $return = array();
+    $coordinates = array();
+    $coordinates_counter = 0;
 
-      foreach ($values as $delta => $columns) {
-          try{
-              $this->prepareValue($delta, $columns);
+    foreach ($values as $delta => $columns) {
+      try {
+        $this->prepareValue($delta, $columns);
 
-              foreach($columns as $key => $items) {
-                  foreach ($items as $item) {
-                      $coordinates[$coordinates_counter][$key][] = $item;
-                  }
-                  $coordinates_counter = 0;
-              }
-
-          } catch (EmptyFeedException $e) {
-              drupal_set_message($e->getMessage(), 'error');
-
-              return FALSE;
-          };
-
-      }
-      if(isset($coordinates)){
-          foreach ($coordinates as $coordinate) {
-              $count_of_coordinates = count($coordinate['lat']);
-
-              for($i = 0; $i < $count_of_coordinates; $i++) {
-                  $return[]['value'] = "POINT (" . $coordinate['lon'][$i] . " " . $coordinate['lat'][$i] . ")";
-              }
+        foreach ($columns as $key => $items) {
+          foreach ($items as $item) {
+            $coordinates[$coordinates_counter][$key][] = $item;
           }
+          $coordinates_counter = 0;
+        }
+
       }
+      catch (EmptyFeedException $e) {
+        drupal_set_message($e->getMessage(), 'error');
 
-      return $return;
+        return FALSE;
+      };
+
+    }
+    if (isset($coordinates)) {
+      foreach ($coordinates as $coordinate) {
+        $count_of_coordinates = count($coordinate['lat']);
+
+        for ($i = 0; $i < $count_of_coordinates; $i++) {
+          $return[]['value'] = "POINT (" . $coordinate['lon'][$i] . " " . $coordinate['lat'][$i] . ")";
+        }
+      }
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function prepareValue($delta, array &$values) {
-        //Here is been preparing values for coordinates
-        foreach ($values as $column => $value) {
-            $separated_coordinates = explode(" ", $value);
-            $values[$column] = array();
+    return $return;
+  }
 
-            foreach ($separated_coordinates as $coordinate) {
-                $values[$column][] = (float) $coordinate;
-            }
-        }
+  /**
+   * {@inheritdoc}
+   */
+  protected function prepareValue($delta, array &$values) {
+    // Here is been preparing values for coordinates.
+    foreach ($values as $column => $value) {
+      $separated_coordinates = explode(" ", $value);
+      $values[$column] = array();
 
-        //Latitude and Longitude should be a pair. If that's false throw EmptyFeedException
-        if(count($values['lat']) != count($values['lon'])) {
-            throw new EmptyFeedException('Latitude and Longitude should be a pair. Change your file and import again.');
-        }
+      foreach ($separated_coordinates as $coordinate) {
+        $values[$column][] = (float) $coordinate;
+      }
     }
+
+    // Latitude and Longitude should be a pair, if not throw EmptyFeedException.
+    if (count($values['lat']) != count($values['lon'])) {
+      throw new EmptyFeedException('Latitude and Longitude should be a pair. Change your file and import again.');
+    }
+  }
+
 }
