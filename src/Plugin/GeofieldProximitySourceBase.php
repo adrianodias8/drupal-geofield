6d2babfcd8239e28b70921f742d46bb94ee0feda
itamair <itamair@1179076.no-reply.drupal.org>
2021-07-22 00:26:02 +0200

    Issue #3223984 by itamair: Notice: Undefined index: lat in Drupal\geofield\Plugin\GeofieldProximitySourceBase->getHaversineOptions()

--- src/Plugin/GeofieldProximitySourceBase.php
+++ src/Plugin/GeofieldProximitySourceBase.php
@@ -172,7 +172,7 @@ abstract class GeofieldProximitySourceBase extends PluginBase implements Geofiel
   public function getHaversineOptions() {
 
     $origin = $this->getOrigin();
-    if (!$origin) {
+    if (!$origin || !isset($origin['lat']) || !isset($origin['lon'])) {
       throw new HaversineUnavailableException('Not able to calculate Haversine Options due to invalid Proximity Origin definition.');
     }
     if ($this->isEmptyLocation($origin['lat'], $origin['lon']) || !$this->isValidLocation($origin['lat'], $origin['lon'])) {
